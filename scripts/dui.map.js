de4645457329ed1b74c1b04bc672805aba5364d4
Miko Gao <gaowhen.com@gmail.com>
2011-08-25 18:15:33 +0800

    push

--- scripts/dui.map.js
+++ scripts/dui.map.js
@@ -2,13 +2,19 @@
  * Author: gaocheng@douban.com
  *
  * TODO: 自定义搜索 control
- * TODO: infoWindow
+ * TODO: 创建地图时，也可以只传入经纬度
  *
  */
 
 (function($) {
 
 	var dui = window.dui || {};
+
+    dui.log = function(obj) {
+        if (typeof(console) != 'undefined' && typeof(console.log) == 'function') {
+            console.log(obj);
+        }
+    };
 
 	dui.iMap = (function() {
 		var mapDefault = {
@@ -22,11 +28,11 @@
 		markerDefault = {
 			lat: mapDefault.lat,
 			lng: mapDefault.lng,
-            latLng: function(){return new google.maps.LatLng(mapDefault.lat, mapDefault.lng)}(),
+			latLng: function(){ return new google.maps.LatLng(mapDefault.lat, mapDefault.lng) }(),
 			draggable: false,
-			handleDrag: function(){},
+			handleDrag: function() {},
 			clickable: false,
-			handleClick: function(){},
+			handleClick: function() {},
 			visible: true
 		},
 		controlDefault = {
@@ -43,16 +49,17 @@
 			scaleControl: true,
 			streetViewControl: false,
 			overviewMapControl: false,
-            searchControl: false
+			searchControl: false
 		},
-        centerDefault = {
-			lat: 39.92,
-			lng: 116.46,
-        },
+		centerDefault = {
+			lat: mapDefault.lat,
+			lng: mapDefault.lng,
+			latLng: function(){ return new google.maps.LatLng(mapDefault.lat, mapDefault.lng) }()
+		},
 		defaults = $.extend(mapDefault, controlDefault);
 
-		function _setCenter(map, latLng) {
-            var opt = $.extend(centerDefault, latLng || {});
+		function _setCenter(map, options) {
+			var opt = $.extend(centerDefault, options || {});
 
 			map.setCenter(opt.latLng);
 		}
@@ -73,8 +80,8 @@
          */
 		function _createMap($ele, options, callback) {
 			var opt = $.extend(defaults, options || {});
-            console.log('_createMap');
-            console.log(opt);
+			dui.log('_createMap');
+			dui.log(opt);
 			$ele.css({
 				'width': opt.width,
 				'height': opt.height
@@ -86,21 +93,21 @@
 			var latlng = new google.maps.LatLng(opt.lat, opt.lng);
 			map.setCenter(latlng);
 
-            console.log(arguments);
+			dui.log(arguments);
 			if (arguments.length === 3 && _isFunction(arguments[2])) {
-                console.log(map);
+				dui.log(map);
 				arguments[2](map);
 			} else if (arguments.length === 2 && _isFunction(arguments[1])) {
-                console.log(map);
+				dui.log(map);
 				arguments[1](map);
 			}
 
-            // 显示搜索控件
-            if(opt.searchControl){
-            }
+			// 显示搜索控件
+			if (opt.searchControl) {
+				_customControlSearch(map);
+			}
 
 			return map;
-
 		}
 
 		/**
@@ -108,74 +115,145 @@
          *
          */
 		function _createMarker(map, options) {
-			var opt = $.extend(markerDefault, options || {});
-            console.log('_createMarker');
-            console.log(opt);
-			var marker = new google.maps.Marker({
-				position: opt.latLng,
-				map: map,
-                draggable: opt.draggable,
-                cilckable: opt.clickable
-			});
+			dui.log('_createMarker');
+			var opt = $.extend(markerDefault, options || {}),
+                marker = new google.maps.Marker({
+                    position: opt.latLng,
+                    map: map,
+                    draggable: opt.draggable,
+                    cilckable: opt.clickable
+                });
 
 			// 如果可拖拽，且有拖拽事件
 			if (opt.draggable && opt.handleDrag && _isFunction(opt.handleDrag)) {
-                google.maps.event.addListener(marker, 'dragend', opt.handleDrag);
+				google.maps.event.addListener(marker, 'dragend', opt.handleDrag);
 			}
 
 			// 如果可点击，且有点击事件
 			if (opt.clickable && opt.handleClick && _isFunction(opt.handleClick)) {
-                google.maps.event.addListener(marker, 'click', opt.handleClick);
+				google.maps.event.addListener(marker, 'click', opt.handleClick);
 			}
 
 			return marker;
 		}
 
-        /** 事件绑定
-         * @map 可以是 map 对象，也可以是 marker 对象
+        /**
+         * 删除 Marker
+         */
+        function _removeMarker(marker){
+            marker.setMap(null);
+        }
+
+		/**
+         * 事件绑定
+         * @ele 可以是 map 对象，也可以是 marker 对象
          * @events 暂时只为 click
          */
-        function _bind(map, events, handle){
-            switch(events) {
+		function _bind(ele, events, handle) {
+			switch (events) {
                 case 'click':
-                    google.maps.event.addListener(map, 'click', handle);
+                    google.maps.event.addListener(ele, 'click', handle);
                     break;
-
                 case 'zoom':
                     // code
                     break;
-
                 case 'drag':
                     // code
                     break;
-
                 default:
                     // code
-            }
+			}
+		}
+
+		/**
+         * 自定义搜索控件
+         *
+         */
+		function _customControlSearch(map) {
+			var $search = $('<div id="search-control"></div>'),
+                $searchUI = $('<div id="search-control-ui"></div>'),
+                $txt = $('<input type="text" name="search-txt" id="search-txt" />'),
+                $btn = $('<input type="button" id="btn-search" value="search" />');
+
+			$searchUI.append($txt).append($btn);
+			$search.append($searchUI);
+
+			map.controls[google.maps.ControlPosition.TOP_LEFT].push($search.get(0));
+			google.maps.event.addDomListener($btn.get(0), 'click', function() {
+                var address = $txt.val();
+                _search(map, address);
+            });
+
+            $txt.bind('keydown', function(e){
+                if(e.keyCode == '13'){
+                    var address = $txt.val();
+                    _search(map, address);
+                }
+            });
+		}
+
+		/**
+         * 搜索
+         *
+         */
+		function _search(map, address) {
+			var geocoder = new google.maps.Geocoder();
+
+			geocoder.geocode({
+                    address: address
+                },
+                function(results, status) {
+                    dui.log(status)
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        map.setZoom(15);
+                        var latLng = results[0].geometry.location;
+                        _createMarker(map, {latLng: latLng});
+                        _setCenter(map, {latLng: latLng});
+                    } else {
+                        // TODO
+                        // 查找不到的处理
+                        alert("Geocode was not successful for the following reason: " + status);
+                    }
+			});
+		}
+
+        /**
+         * info window
+         */
+        function _infowindow(map, marker, content){
+            var infowindow = new google.maps.InfoWindow({content: content});
+
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.open(map, marker);
+            });
         }
 
 		return {
 			createMap: function($element, options) {
 				return _createMap.apply(this, arguments);
 			},
-            setCenter: function(map, latLng){
-                _setCenter(map, latLng);
-            },
+			setCenter: function(map, options) {
+				_setCenter(map, options);
+			},
 			createMarker: function(map, options) {
 				return _createMarker(map, options);
 			},
-			removeMarker: function() {},
-			moveMarker: function() {},
+			removeMarker: function(marker) {
+                _removeMarker(marker);
+            },
+            infowindow: function(map, marker, content){
+                _infowindow(map, marker, content);
+            },
 			init: function($element, options, callback) {
 				return _createMap.apply(this, arguments);
 				//return _createMap($element, options, callback);
 			},
-            bind: function(map, events, handle){
-                _bind(map, events, handle);
-            },
-            search: function(){
-                return _search();
-            }
+			bind: function(map, events, handle) {
+				_bind(map, events, handle);
+			},
+			search: function(address) {
+				return _search(address);
+			}
 		};
 	})();
 
